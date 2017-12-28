1f146ed0f7c17cbaa8de9dd6e6a3e85eb790b4b4
Andreas Stephan <andreas.stephan@internetx.com>
2017-12-28 15:10:26 +0100

    book controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,122 +6,113 @@ use App\Book;
 use App\Publisher;
 use Illuminate\Http\Request;
 
-class BookController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-	    $books = Book::orderBy('title', 'asc')->paginate(10);
-	    return view('book.index')->with('books', $books);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    	$publishers = Publisher::orderBy('name', 'asc')->get();
-	    return view('book.create')->with('publishers', $publishers);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-	    $this->validate( $request, [
-		    'title' => 'required|min:1|max:121',
-		    'publishedAt' => 'required|min:1|max:121',
-		    'publisher' => 'required|min:1|max:121',
-	    ] );
-	    
-	 
-	    $publisher = Publisher::find($request->input("publisher"));
-	    
-	    // TODO
-	    // if not found withErrors
-	    // datefield
-	    
-	    Book::create( [
-		    'title'    => $request->input( 'title' ),
-		    'publisher_id'    => $request->input( 'publisher' ),
-		    'published_at'    => $request->input( 'publishedAt' )
-	    ] );
+class BookController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		$books = Book::orderBy('title', 'asc')->paginate(10);
+		return view('book.index')->with('books', $books);
+	}
 	
-	    return redirect()->route( 'book.index' )->with( 'message', 'Book created successfully' );
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-	    return view('book.show')->with([
-		    'book' => $book
-	    ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Book $book)
-    {
-	    return view('book.edit')->with([
-		    'book' => $book,
-		    'publishers' => Publisher::orderBy('name', 'asc')->get()
-	    ]);
-	    
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-	    // TODO: integer, isDate
-	    $this->validate( $request, [
-		    'title' => 'required|min:1|max:121',
-		    'publisher' => 'required|min:1|max:121',
-		    'publishedAt' => 'required|min:1|max:121'
-	    ] );
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		$publishers = Publisher::orderBy('name', 'asc')->get();
+		return view('book.create')->with('publishers', $publishers);
+	}
 	
-	    $book->title = $request->input( 'title' );
-	    $book->publisher_id = $request->input( 'publisher' );
-	    $book->published_at = $request->input( 'publishedAt' );
-	    $book->save();
-	    
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		$this->validate($request, [
+			'title' => 'required|min:1|max:121',
+			'publisher' => 'required|integer',
+			'publishedAt' => 'required|date_format:"Y,m,d"'
+		]);
+		
+		
+		$publisher = Publisher::find($request->input("publisher"));
+		
+		// TODO
+		// if not found withErrors
+		// datefield
+		
+		Book::create([
+			'title' => $request->input('title'),
+			'publisher_id' => $request->input('publisher'),
+			'published_at' => $request->input('publishedAt')
+		]);
+		
+		return redirect()->route('book.index')->with('message', 'Book created successfully');
+	}
 	
-	    return redirect()->route( 'book.index' )->with( 'message', 'Book updated successfully' );
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-	    $book->delete();
-	    return redirect()->route( 'book.index' )->with( 'message', 'Book deleted successfully' );
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Book $book
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Book $book) {
+		return view('book.show')->with([
+			'book' => $book
+		]);
+	}
+	
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Book $book
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Book $book) {
+		return view('book.edit')->with([
+			'book' => $book,
+			'publishers' => Publisher::orderBy('name', 'asc')->get()
+		]);
+		
+	}
+	
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \App\Book $book
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Book $book) {
+		$this->validate($request, [
+			'title' => 'required|min:1|max:121',
+			'publisher' => 'required|integer',
+			'publishedAt' => 'required|date_format:"Y,m,d"'
+		]);
+		
+		$book->title = $request->input('title');
+		$book->publisher_id = $request->input('publisher');
+		$book->published_at = $request->input('publishedAt');
+		$book->save();
+		
+		
+		return redirect()->route('book.index')->with('message', 'Book updated successfully');
+	}
+	
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Book $book
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Book $book) {
+		$book->delete();
+		return redirect()->route('book.index')->with('message', 'Book deleted successfully');
+	}
 }
