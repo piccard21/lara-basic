cd13b95db6e7eefa9bbc94cd4aff58b26672b21f
cafe-serendipity <cafe.serendipity21@gmail.com>
2017-12-28 21:18:22 +0100

    show book

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -9,11 +9,36 @@
 			<p class="lead">show</p>
 		</div>
 
-		<div class="card" style="width: 20rem;">
-			<div class="card-body">
-				<h4 class="card-title">Book: {{ $book->title }}</h4>
-				<p class="card-text">Publisher: {{ $book->publisher->name }}</p>
-				<p class="card-text">Published at: {{ $book->published_at }}</p>
+		<div class="row">
+			<div class="col-sm">
+				<h2>{{ $book->title }}</h2>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col-sm">
+				<table class="table table-striped">
+					<thead>
+					<tr>
+						<th scope="col">Author</th>
+						<th scope="col">Publisher</th>
+						<th scope="col">Date</th>
+					</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{{ $book->authors->implode('lastname', '/')  }}</td>
+							<td>{{ $book->publisher->name }}</td>
+							<td>{{ $book->published_at }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm">
 				<a href="{{ route("book.index") }}" class="btn btn-primary">back</a>
 			</div>
 		</div>
