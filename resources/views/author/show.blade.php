2e8b38c81fa5702bc21271dac68633b8c1aeac66
cafe-serendipity <cafe.serendipity21@gmail.com>
2017-12-28 21:07:45 +0100

    show author

--- resources/views/author/show.blade.php
+++ resources/views/author/show.blade.php
@@ -1,22 +1,53 @@
 @extends('layout.app')
 
 @section('content')
-	<div class="container">
+    <div class="container">
 
-		<div class="jumbotron jumbo-custom">
-			<h1 class="display-4">Author </h1>
-			<hr>
-			<p class="lead">show</p>
-		</div>
+        <div class="jumbotron jumbo-custom">
+            <h1 class="display-4">Author </h1>
+            <hr>
+            <p class="lead">show</p>
+        </div>
 
-		<div class="card" style="width: 20rem;">
-			<div class="card-body">
-				<h4 class="card-title">Author: {{ $author->id }}</h4>
-				<p class="card-text">{{ $author->lastname }}, {{ $author->forename }}</p>
-				<a href="{{ route("author.index") }}" class="btn btn-primary">back</a>
-			</div>
-		</div>
-	</div>
+        <div class="row">
+            <div class="col-sm">
+                <h2>{{ $author->lastname }}, {{ $author->forename }}</h2>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-sm">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Publisher</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($author->books()->get() as $book)
+                        <tr>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->published_at }}</td>
+                            <td>{{ $book->publisher->name }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+                <a href="{{ route("author.index") }}" class="btn btn-primary">back</a>
+            </div>
+        </div>
+
+
+
+    </div>
 @endsection
 
 
