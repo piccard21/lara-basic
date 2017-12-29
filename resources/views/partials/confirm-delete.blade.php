fce8221fe8fa568f46d80033743671a3cb268c58
Andreas Stephan <andreas.stephan@internetx.com>
2017-12-29 16:58:39 +0100

    confirm blade

--- resources/views/partials/confirm-delete.blade.php
+++ resources/views/partials/confirm-delete.blade.php
@@ -4,13 +4,13 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Delete example</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-footer">
-                <a class="btn btn-outline-secondary" data-dismiss="modal">Close</a>
+                <a class="btn btn-outline-secondary" data-dismiss="modal">Cancel</a>
                 <a id="btn-confirm-delete-ok" class="btn btn-danger">Delete</a>
             </div>
         </div>
