076d23d9dbcf533fe42ac86829784820adcea88f
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-01-01 22:53:04 +0100

    roles seed

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+	public function run() {
+		$author = Role::create( [
+			'name'        => 'Author',
+			'slug'        => 'author',
+			'permissions' => [
+				'create-post' => true,
+			]
+		] );
+		$editor = Role::create( [
+			'name'        => 'Editor',
+			'slug'        => 'editor',
+			'permissions' => [
+				'update-post'  => true,
+				'publish-post' => true,
+			]
+		] );
+    }
+}
